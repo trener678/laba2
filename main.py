f1b6118fe0cab894f6abb53c50f907da923bbb3b
trener678 <vortex9a@gmail.com>
2025-11-14 10:29:10 +0300

    create pr

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-10, 10, 100)
+y = x**2
+
+plt.plot(x, y)
+plt.title('y = x²')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+plt.show()
